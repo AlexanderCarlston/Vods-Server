1e1e8a20099e8c3c2dad2e1085445e63bed003ae
alekscarlston@yahoo.com <alekscarlston@gmail.com>
2019-12-03 19:03:14 -0700

    Add games request and dotenv

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 require('dotenv').config()
 
-app.get('/', function (req, res) {
- return res.send(process.env.CLIENT_ID);
+const config = {
+  headers: {
+    'Client-ID': process.env.CLIENT_ID
+  }
+}
+const baseUrl = 'https://api.twitch.tv/helix/'
+
+app.get('/games', function (req, res) {
+  getGames().then(data => res.json(data))
 });
 
-app.get('/disocvery', function(req, res) {
-    return res.send('example')
+app.get('/disocvery', function (req, res) {
+  return res.send('example')
 });
 
 app.listen(process.env.PORT || 8080);
 
+function getGames() {
+  return axios.get(baseUrl + 'games/top', config).then(response => {
+      console.log(response.data);
+      return response.data
+    })
+}
